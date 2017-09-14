07134dcd32b8d7d4642d799a3729d8f4a40f68af
Tatjana Kuznetsova <kuznetsovatatjana@users.noreply.github.com>
2017-09-14 10:47:04 +0300

    esimene php rida

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tatjana Kuznetsova Veebileht</title>	
+</head>
+
+<body>
+	<h1>Tatjana Kuznetsova programmeerib veebi</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	
+<?php
+	echo "Algas PHP õppimine."; 
+?>
+
+</body>
+</html>
